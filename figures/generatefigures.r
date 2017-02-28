b094bdd85fa79a8d7d87dc610e184f2ecd195fd4
shramdas <shwetaramdas@gmail.com>
2017-02-27 22:15:28 -0500

    Code to generate figures in the paper

---
+++ figures/generatefigures.r
@@ -0,0 +1,56 @@
+FOLDER = "C:/Users/sramdas/Dropbox/"
+
+expn = read.csv(paste(FOLDER, "TM_Li//original_data/TMtissueAge RMA JunLi.txt",sep=""),skip=1,header=T,stringsAsFactors = F,sep="\t")
+info = read.table(paste(FOLDER,"TM_Li/redo_analysis/aug2016/NEWANALYSIS_Nov22_2016/TM datasheetinfo_tissue_revised.txt",sep=""),header=T,sep="\t",stringsAsFactors = F)
+
+info2 = info[order(info$Cel.Filename),]
+library(limma)
+library("preprocessCore")
+
+#supplementary figure 1
+plot(info2$Age, info2$RIN,col=as.factor(info2$type),pch=19,main="Plotting sample age versus RIN (red: remnant, black: cornea)",cex.main=0.9)
+
+#heatmap
+rowstokeep = which(limma_5pcs_pval < 0.0001)
+genestoplot = unique(genes[rowstokeep])
+redblue100<-rgb(read.table("redblue100.txt",sep='\t',row.names=1,header=T))
+toplot = RESIDUALS[rowstokeep,]
+
+heatmap(t(toplot), col=redblue100,labRow=info2$Age, labCol=genestoplot)
+#standardize
+library(MASS)
+toplot2 = scale(t(toplot))
+toplot2 = toplot2[order(info2$Age),]
+heatmap(toplot2, col=redblue100,labRow=sort(info2$Age), labCol=genestoplot,Rowv = NA)
+
+#panel on right
+
+
+#figure 2
+#creating plot of expression residuals (i.e. expression corrected for covariates) by age
+
+
+op <- par(mfrow = c(3,3),mai=c(0.4,0.35,0.5,0.35))
+
+for(gene in genestoplot){
+  rowstoplot = which(genes[rowstokeep]==gene)
+  if(length(rowstoplot)> 1){
+    rowstoplot = rowstoplot[1]
+  }
+  plot(as.numeric(RESIDUALS[rowstokeep[rowstoplot],]),info2$Age,xlab="Residuals",ylab="Age (years)",main=gene,cex.main=0.9,pch=19)
+  abline(lm(info2$Age ~ as.numeric(RESIDUALS[rowstokeep[rowstoplot],])))
+}
+
+library(ggplot2)
+plots = list()
+i = 1
+for(gene in genestoplot){
+  rowstoplot = which(genes[rowstokeep]==gene)
+  if(length(rowstoplot)> 1){
+    rowstoplot = rowstoplot[1]
+  }
+   datamat = as.data.frame(cbind(RESIDUALS[rowstokeep[rowstoplot],], info2$Age))
+   p[[i]] <- ggplot(datamat, aes(x=V1,y=V2)) + geom_point() +geom_smooth(method=lm,se=FALSE) + scale_x_continuous(name="Residuals") + scale_y_continuous(name="Age(yrs)") + ggtitle(gene) + theme(plot.title=element_text(size=9),axis.title=element_text(size=8))
+   i = i +1
+}
+grid.arrange(p[[1]],p[[2]],p[[3]],p[[4]],p[[5]],p[[6]],p[[7]],p[[8]],p[[9]],ncol=3)
